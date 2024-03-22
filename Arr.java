53668c279d7b1dd3e9e067febd243eb77ce836c3
sheeba0809 <158248598+sheeba0809@users.noreply.github.com>
2024-03-22 09:23:17 +0530

    Arrtask

---
+++ Arr.java
@@ -0,0 +1,14 @@
+class Arr{
+public static void main(String[] args){
+arr ob=new arr();
+main();
+}
+public static  void main(){
+int i=0;
+String arr[]={"1","2","3","4","sheeba"};
+
+for(i=0;i<arr.length;i++){
+System.out.println(arr[i]);
+}
+}
+}
