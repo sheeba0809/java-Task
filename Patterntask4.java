65525e84b7de72f428d7b9a04b7129a2d83472e7
sheeba0809 <158248598+sheeba0809@users.noreply.github.com>
2024-03-23 10:46:51 +0530

    pattern4

---
+++ Patterntask4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Patterntask4{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++){
+for(int j=1;j<=n;j++){
+if(i==1 || i==n){
+System.out.print("*");
+}
+else if(j==3){
+System.out.print("*");
+}
+else{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+
+}
