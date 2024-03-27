2549871fab5a7c7c7a541f8a3e149371ef9db563
sheeba0809 <158248598+sheeba0809@users.noreply.github.com>
2024-03-27 15:37:58 +0530

    cmdlineargs

---
+++ CmdlineArgs.java
@@ -0,0 +1,12 @@
+class CmdlineArgs{
+public static void main(String args[]){
+String Busname=args[0];
+int Noofseats=Integer.parseInt(args[1]);
+int BookedSeats=Integer.parseInt(args[2]);
+int AvailableSeats=Integer.parseInt(args[3]);
+System.out.println("Busname: "+Busname+" "+"Noofseats: "+Noofseats+" "+ "BookedSeats: "+BookedSeats+" "+ "AvailableSeats: "+ AvailableSeats+" ");
+
+
+}
+
+}
