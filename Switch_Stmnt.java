9c23fdee71f9086d79e4544c271755f18e99def4
sheeba0809 <158248598+sheeba0809@users.noreply.github.com>
2024-04-01 11:08:13 +0530

    statement

---
+++ Switch_Stmnt.java
@@ -0,0 +1,19 @@
+class Switch_Stmnt{
+public static void main(String args[]){
+int i=10;
+switch(expession){
+case expession-1:
+//statement
+break;
+case expession2:
+//statement
+break;
+case expession3:
+//statement
+break;
+default:
+//statement
+break;
+}
+}
+}
