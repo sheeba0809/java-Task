f9b603e0d5f809b663b0a4f64eddd2102875943d
sheeba0809 <158248598+sheeba0809@users.noreply.github.com>
2024-04-06 12:24:01 +0530

    userinput4

--- Emctask4.java
+++ Emctask4.java
@@ -3,7 +3,12 @@ class Task4{
 public static void main(String args[]){
     Scanner scan = new Scanner(System.in);
     String Name = scan.nextLine();
-    
+    double Score = scan.nextDouble();
+    scan.nextLine();
+    String Department = scan.nextLine();
+    System.out.println("My name is "+Name);
+    System.out.println("My score is "+Score/10);
+    System.out.println("My department is "+Department);
 }
 
 }
